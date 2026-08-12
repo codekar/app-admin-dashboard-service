4656e07dfb4b04120b63a10728ad77046d6f6345
sham_bhujbal <sbhujbal03@gmail.com>
2026-08-12 22:07:51 +0530

    Initial commit

---
+++ src/main/java/com/company/store/AppAdminDashboardServiceApplication.java
@@ -0,0 +1,11 @@
+package com.company.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAdminDashboardServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppAdminDashboardServiceApplication.class, args);
+    }
+}
